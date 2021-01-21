2b1e6135378185bd2b2ed576d79f53b5644231cf
Sam Bryant <sam.bryant@armakuni.com>
2021-01-21 15:07:37 +0000

    SQS messages differ for lambda events and receive message

--- spp_contract_testing/sqs.py
+++ spp_contract_testing/sqs.py
@@ -6,6 +6,19 @@ from schema import And, Schema, Use
 def wrap_schema_for_sqs(contract_schema: Schema) -> Schema:
     return Schema(
         {
+            "Messages": [
+                Schema(
+                    {"body": And(Use(json.loads), contract_schema)},
+                    ignore_extra_keys=True,
+                ),
+            ],
+        },
+        ignore_extra_keys=True,
+    )
+
+def wrap_schema_for_sqs_event(contract_schema: Schema) -> Schema:
+    return Schema(
+        {
             "Records": [
                 Schema(
                     {"body": And(Use(json.loads), contract_schema)},
